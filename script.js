298ee807bfff899cff452b94c10550a23c9a90a8
Gustavocasual <themarketigdig@gmail.com>
2024-08-14 16:30:44 -0300

    Gustavocasual/IvanildoOfficial

---
+++ script.js
@@ -0,0 +1,61 @@
+function vouteadorar(){
+    window.location="https://youtu.be/wtyaFEAR8x8?si=UdEYLGMN2b9RS-IC";
+}
+
+function esquerdo(){ 
+    var conteudodetrabalho = document.querySelector(".conteudodetrabalho");
+    conteudodetrabalho.scrollBy({
+        left: -345, behavior: "smooth"
+        
+    }) ;
+    
+}
+
+
+function direito(){
+   
+    var conteudodetrabalho = document.querySelector(".conteudodetrabalho");
+    conteudodetrabalho.scrollBy({
+        left: 345, behavior: "smooth"
+    });
+
+}
+function esquerdocelular(){ 
+    var conteudodetrabalhocelular = document.querySelector(".conteudodetrabalhocelular");
+    conteudodetrabalhocelular.scrollBy({
+        left: -345, behavior: "smooth"
+        
+    }) ;
+    
+}
+
+
+function direitocelular(){
+   
+    var conteudodetrabalhocelular = document.querySelector(".conteudodetrabalhocelular");
+    conteudodetrabalhocelular.scrollBy({
+        left: 345, behavior: "smooth"
+    });
+
+}
+
+
+function linkwhat(){
+    window.location = "https://wa.me/5511912849922";
+}
+function instagrem(){
+    window.location = "https://www.instagram.com/ivanildoalvesoficiall/";
+}
+function face(){
+    window.location = "https://www.facebook.com/alvessivanildo";
+}
+
+
+function sup(){
+    window.location = "https://wa.me/+5511942442175";
+}
+
+
+function ceo(){
+    window.location = "https://www.instagram.com/spectro_tec/";
+}
